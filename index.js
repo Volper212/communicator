cab79145baa7beda37dd144eefd75c6f7a8bfbc2
Volper212 <volpergaming@gmail.com>
2021-01-26 17:10:30 +0100

    Finish backend

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const form = document.querySelector('.message-form');
+const messages = document.querySelector('.messages');
 const input = form.querySelector('#message');
+const nick = form.querySelector('#nick');
 
-const socket = new WebSocket('ws://localhost:8000/server.php');
+const socket = new WebSocket('ws://localhost:8000');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    const message = input.value;
+    socket.send(JSON.stringify({ [nick.value]: input.value }));
     input.value = '';
 });
 
 socket.addEventListener('message', message => {
-    console.log(message.data);
+    const data = JSON.parse(message.data);
+    const p = document.createElement('p');
+    const nick = Object.keys(data)[0];
+    p.textContent = `<${nick}> ${data[nick]}`;
+    messages.append(p);
 });
--- server.php
+++ server.php
@@ -1,44 +1,87 @@
 <?php
-$server = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_set_option($server, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($server, "127.0.0.1", 8000);
+function between(int $number, int $min, int $max) {
+    return $number >= $min && $number <= $max;
+}
+
+function mask($text)
+{
+	$b1 = 129;
+	$length = strlen($text);
+	
+	if ($length <= 125)
+		$header = pack('CC', $b1, $length);
+	elseif ($length > 125 && $length < 65536)
+		$header = pack('CCn', $b1, 126, $length);
+	elseif ($length >= 65536)
+		$header = pack('CCNN', $b1, 127, $length);
+	return $header . $text;
+}
+
+function unmask(string $text) {
+    $length = ord($text[1]) & 127;
+    $offset = 2;
+    if ($length === 126) {
+        $offset = 4;
+    } elseif ($length === 127) {
+        $offset = 10;
+    }
+    $masks = substr($text, $offset, 4);
+    $data = substr($text, $offset + 4);
+	$text = "";
+	for ($i = 0; $i < strlen($data); ++$i) {
+		$text .= $data[$i] ^ $masks[$i % 4];
+    }
+	return $text;
+}
+
+$server = stream_socket_server("tcp://127.0.0.1:8000");
 
 $clients = [];
 
-class Listener extends Thread {
-    public function run() {
-        global $server;
-        global $clients;
-        while (true) {
-            socket_listen($server);
-            $client = socket_accept($server);
+$file = "messages.json";
 
-            $request = socket_read($client, 5000);
-            preg_match('#Sec-WebSocket-Key: (.*)\r\n#', $request, $matches);
-            $key = base64_encode(pack(
-                'H*',
-                sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-            ));
+$exists = file_exists($file);
 
-            $headers = "HTTP/1.1 101 Switching Protocols\r\n"
-                . "Upgrade: websocket\r\n"
-                . "Connection: Upgrade\r\n"
-                . "Sec-WebSocket-Version: 13\r\n"
-                . "Sec-WebSocket-Accept: $key\r\n\r\n";
-            socket_write($client, $headers, strlen($headers));
-            $clients[] = $client;
-        }
-    }
-}
-
-$listener = new Listener;
-$listener->start();
+$messages = $exists ? json_decode(file_get_contents($file), true, 3) : [];
 
 while (true) {
-    sleep(1);
-    $message = "bruh";
-    foreach ($clients as $client) {
-        socket_write($client, chr(129) . chr(strlen($message)) . $message);
+    $changed = [$server];
+    $null = null;
+    stream_select($changed, $null, $null, 0, 10);
+    if (in_array($server, $changed)) {
+        $client = stream_socket_accept($server);
+        preg_match('#Sec-WebSocket-Key: (.*)\r\n#', fread($client, 5000), $matches);
+        $key = base64_encode(pack(
+            'H*',
+            sha1($matches[1] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+        ));
+
+        fwrite($client, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Version: 13\r\nSec-WebSocket-Accept: $key\r\n\r\n");
+        stream_set_timeout($client, 0);
+        foreach ($messages as $message) {
+            fwrite($client, mask(json_encode($message, true, 2)));
+        }
+        $clients[] = $client;
+    }
+    foreach ($clients as $index => $client) {
+        if ($result = stream_get_contents($client)) {
+            $text = unmask($result);
+            if ($text === "" || (strlen($text) === 2 && between(hexdec(bin2hex($text)), 1000, 1015))) {
+                unset($clients[$index]);
+                continue;
+            }
+            $decoded = json_decode($text, true, 2);
+            if ($decoded // Check if json_decode succeeded
+                && $text[0] === "{" // Check if object
+                && count($decoded) === 1 // Check if only one key value pair
+                && gettype($decoded[array_key_first($decoded)]) === "string") {
+                foreach ($clients as $client) {
+                    fwrite($client, mask($text));
+                }
+                $messages[] = $decoded;
+                file_put_contents($file, json_encode($messages, 0, 3));
+            }
+        }
     }
 }
 ?>
